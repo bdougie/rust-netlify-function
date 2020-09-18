50132b0b235b0a4ab5bb5b692aded72559f99af5
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2020-09-18 06:35:26 -0700

    hello world

---
+++ functions/hello/hello.js
@@ -0,0 +1,14 @@
+const { hello_world } = require('./pkg/hello');
+exports.handler = async function({ queryStringParameters = {} }) {
+  try {
+    const { name = 'World' } = queryStringParameters;
+    const message = hello_world(name);
+    return {
+      statusCode: 200,
+      body: message,
+    };
+  } catch (error) {
+    console.log(error);
+    return { statusCode: error.statusCode || 500, body: error.message };
+  }
+};
